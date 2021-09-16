abfe9c450f6b1860fa4f922e76655dfbf69eb87c
Kata Martin <katasm@gmail.com>
2021-09-16 16:16:10 -0700

    Handle unexpected color modes

--- src/use-colormap.mjs
+++ src/use-colormap.mjs
@@ -6,13 +6,22 @@ import generator from './generator'
 const useColormap = (name, count = 255, format = 'rgb') => {
   const [mode] = useColorMode()
 
+  let colorMode = mode
+
+  if (!['light', 'dark'].includes(mode)) {
+    console.warn(
+      `Unexpected \`theme-ui-color-mode\`, ${mode}. Using \`dark\` as fallback.`
+    )
+    colorMode = 'dark'
+  }
+
   if (!colormaps.map((d) => d.name).includes(name)) {
     throw Error(`requested colormap '${name}' is not defined`)
   }
 
   const colormap = useMemo(() => {
-    return generator(name, mode, count, format)
-  }, [name, mode])
+    return generator(name, colorMode, count, format)
+  }, [name, colorMode])
 
   return colormap
 }
