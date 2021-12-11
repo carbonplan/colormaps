653954363c3d41b5f7bad2da97aab37be8b7d9f5
freeman-lab <the.freeman.lab@gmail.com>
2021-12-11 11:50:21 -0800

    add themed hook

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,4 @@
 export { default as useColormap } from './use-colormap'
+export { default as useThemedColormap } from './use-themed-colormap'
 export { default as makeColormap } from './make-colormap'
 export { default as colormaps } from './colormaps'
