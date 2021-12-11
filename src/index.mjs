ed7e90c27f85ef8c758d151610f0244e03e6e52d
freeman-lab <the.freeman.lab@gmail.com>
2021-12-10 17:25:54 -0800

    refactor handling of theme

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,3 @@
 export { default as useColormap } from './use-colormap'
+export { default as makeColormap } from './make-colormap'
 export { default as colormaps } from './colormaps'
-export { default as Colorbar } from './colorbar'
